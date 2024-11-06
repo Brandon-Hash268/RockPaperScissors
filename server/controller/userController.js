2ec4c0a5f2d51abd88b47b16d9a814df02406be3
malfinnr <malfinnr98@gmail.com>
2024-11-06 15:40:42 +0700

    feat: add User

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,30 +1,43 @@
-const { User } = require("../models")
+const { User } = require("../models");
 
 class UserController {
-    static async login(req, res, next){
-        try {
-            const { userName } = req.body 
-            const [ user, created ] = await User.findOrCreate({
-                where: { userName },
-                default: {
-                    win: 0,
-                    lose: 0,
-                },
-            })
+  static async login(req, res, next) {
+    try {
+      const { userName } = req.body;
+      const [user, created] = await User.findOrCreate({
+        where: { userName },
+        default: {
+          win: 0,
+          lose: 0,
+        },
+      });
 
-            return res.status(200).json({
-                message: created ? 'User created successfully' : 'User found',
-                user: {
-                  id: user.id,
-                  userName: user.userName,
-                },
-              });
+      return res.status(200).json({
+        message: created ? "User created successfully" : "User found",
+        user: {
+          id: user.id,
+          userName: user.userName,
+        },
+      });
+    } catch (err) {
+      console.log("🚀 ~ UserController ~ register ~ err:", err);
+      next(err);
+    }
+  }
+  static async getUser(req, res, next) {
+    try {
+      const { id } = req.body;
+      const user = await User.findByPk(id);
 
-        } catch (err) {
-            console.log("🚀 ~ UserController ~ register ~ err:", err)
-            next(err)
-        }
-    } 
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.status(200).json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
