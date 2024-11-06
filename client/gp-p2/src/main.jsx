77516535bdc573c54c136b89454e0d63e2bee3a2
AlifNaufaldo <muhammadalifnaufaldo@gmail.com>
2024-11-06 14:56:49 +0700

    Added Client:Context

--- client/gp-p2/src/main.jsx
+++ client/gp-p2/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import './index.css'
 import App from "./App.jsx";
+import { LanguageProvider } from "./context/LanguageContext.jsx"; 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <LanguageProvider> 
+      <App />
+    </LanguageProvider>
   </StrictMode>
 );
